1f1fa788e69eb97b297104e67f5c530c3f85358e
PuffsCoin <abvhiael@leafycauldronapothecary.com>
2019-05-22 16:18:57 -0300

    Update dbManager.ts

--- src/dbManager.ts
+++ src/dbManager.ts
@@ -14,7 +14,7 @@ import {
 import BN = require('bn.js')
 
 const level = require('level-mem')
-const Block = require('ethereumjs-block')
+const Block = require('puffscionjs-block')
 
 /**
  * Abstraction over a DB to facilitate storing/fetching blockchain-related
